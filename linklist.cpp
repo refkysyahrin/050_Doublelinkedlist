1c3624b209722f9fac849a2976364589a1f0e793
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-07-11 10:15:47 +0700

    bool listEmty

--- linklist.cpp
+++ linklist.cpp
@@ -131,4 +131,23 @@ void deleteNode()
 
 }
 
-		
+bool listEmty()
+    {
+        return (START == NULL);
+    }
+
+    void traverse()
+    {
+        if (listEmty())
+            cout << "\nList is emty" << endl;
+        else
+        {
+            cout << "\nRecord in accending order of roll number are:" << endl;
+            Node* currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << " " << currentNode->name << endl;
+                currentNode = currentNode->next;
+            }
+        }
+    }	
