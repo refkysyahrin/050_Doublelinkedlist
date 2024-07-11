035db9b8ef56017a75fe6f3370a3e061548f257a
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-07-11 10:10:52 +0700

    void addNode

--- linklist.cpp
+++ linklist.cpp
@@ -9,3 +9,12 @@ struct Node {
 };
 
 Node* START = NULL;
+
+void addNode()
+{
+	Node* newNode = new Node(); // step 1: create a new node
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs; // assign value to the data field of the new node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; // assign value to the data field of the new node
+    
