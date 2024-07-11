9148327e44642ac51b8379d16f71d3c0fe740a19
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-07-11 10:14:42 +0700

    bool search

--- linklist.cpp
+++ linklist.cpp
@@ -67,5 +67,15 @@ void addNode()
 	}
 }
 
-
+bool search(int rollNo, Node** previous, Node** current)
+{
+	*previous = NULL;
+	*current = START;
+	while (*current != NULL && (*current)->noMhs != rollNo)
+	{
+		*previous = NULL;
+		*current = START;
+	}
+	return (*current != NULL);
+}
 		
