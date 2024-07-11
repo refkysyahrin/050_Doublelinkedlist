d8298426c37c951351fa4436b358da4f27bca1f5
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-07-11 10:16:18 +0700

    void revtraverse

--- linklist.cpp
+++ linklist.cpp
@@ -151,3 +151,22 @@ bool listEmty()
             }
         }
     }	
+
+    void revtraverse()
+	{
+		if (listEmty())
+			cout << "\nList is emty" << endl;
+		else
+		{
+			cout << "\nRecords in descending order pf roll number are:" << endl;
+			Node* currentNode = START;
+			while (currentNode->next != NULL)
+				currentNode = currentNode->next;
+
+			while (currentNode != NULL)
+			{
+				cout << currentNode->noMhs << " " << currentNode->name << endl;
+				currentNode = currentNode->prev;
+			}
+		}
+	}
