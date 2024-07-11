f37dc93bcfa4cc36f890d2f496ab72f2cbdae4b5
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-07-11 10:16:50 +0700

    void searchData

--- linklist.cpp
+++ linklist.cpp
@@ -170,3 +170,24 @@ bool listEmty()
 			}
 		}
 	}
+
+void searchData()
+	{
+		if (listEmty() == true)
+		{
+			cout << "\nlist is emty" << endl;
+		}
+		Node* prev, * curr;
+		prev = curr = NULL;
+		cout << "\nEnter the roll numbber of the student whose record you want to search: "; int num;
+		cin >> num;
+		if (search(num, &prev, &curr) == false)
+			cout << "\nRecord not found" << endl;
+		else
+		{
+			cout << "\nRecord found" << endl;
+			cout << "\nRoll number" << curr->noMhs << endl;
+			cout << "\nName: " << curr->name << endl;
+		}
+
+	}
