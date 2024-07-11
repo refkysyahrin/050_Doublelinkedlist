54dab94e517b634bf562355fb58c34353e1a0a78
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-07-11 10:12:08 +0700

    insert the new node in the middle or at the end

--- linklist.cpp
+++ linklist.cpp
@@ -17,4 +17,29 @@ void addNode()
 	cin >> newNode->noMhs; // assign value to the data field of the new node
 	cout << "\nEnter the name of the student: ";
 	cin >> newNode->name; // assign value to the data field of the new node
-    
+
+
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+	{// step 2: insert the new node at the beginning
+		if (START != NULL && newNode->noMhs == START->noMhs)
+		{
+			cout << "\033[31mDuplicate roll number not allowed\033[0m" << endl;
+			return;
+		}
+		// if the list is emty, make the new node the START
+		newNode->next = START; // step 3: make the new node point to the first node
+		if (START != NULL)
+		{
+			START->prev = newNode; // step 4: make the first node to the new node
+		}
+		newNode->prev = NULL; // step 5: make the new node point to NULL
+		START = newNode; // step 6: make the new node the first node
+	}
+    else
+	{
+		// insert the new node in the middle or at the end
+		Node* current = START; // step 1.a: start from the first node
+		Node* previous = NULL; // step 1.b: previous node is NULL initially
+
+		
+}
